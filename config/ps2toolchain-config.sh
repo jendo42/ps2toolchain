4c8c92e6fc4469c1e6daf084b3efc438c4c97efd
Michal Jenikovsky <jendo@jmsystems.sk>
2024-10-06 18:55:32 +0200

    point repo references to devel [FORK]
    Signed-off-by: Michal Jenikovsky <jendo@jmsystems.sk>

--- config/ps2toolchain-config.sh
+++ config/ps2toolchain-config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-PS2TOOLCHAIN_DVP_REPO_URL="https://github.com/ps2dev/ps2toolchain-dvp.git"
-PS2TOOLCHAIN_DVP_DEFAULT_REPO_REF="main"
-PS2TOOLCHAIN_IOP_REPO_URL="https://github.com/ps2dev/ps2toolchain-iop.git"
-PS2TOOLCHAIN_IOP_DEFAULT_REPO_REF="main"
-PS2TOOLCHAIN_EE_REPO_URL="https://github.com/ps2dev/ps2toolchain-ee.git"
-PS2TOOLCHAIN_EE_DEFAULT_REPO_REF="main"
+PS2TOOLCHAIN_DVP_REPO_URL="https://github.com/jendo42/ps2toolchain-dvp.git"
+PS2TOOLCHAIN_DVP_DEFAULT_REPO_REF="MJE-devel"
+PS2TOOLCHAIN_IOP_REPO_URL="https://github.com/jendo42/ps2toolchain-iop.git"
+PS2TOOLCHAIN_IOP_DEFAULT_REPO_REF="MJE-devel"
+PS2TOOLCHAIN_EE_REPO_URL="https://github.com/jendo42/ps2toolchain-ee.git"
+PS2TOOLCHAIN_EE_DEFAULT_REPO_REF="MJE-devel"
 
 if test -f "$PS2DEV_CONFIG_OVERRIDE"; then
   source "$PS2DEV_CONFIG_OVERRIDE"
